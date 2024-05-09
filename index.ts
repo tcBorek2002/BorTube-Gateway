6740601cd68c8a74dd1dc1f92646580599af888a
Borek Bandell <borekbandell@hotmail.com>
2024-05-09 12:17:15 +0200

    Changed cors to specific url instead of wildcard *

--- index.ts
+++ index.ts
@@ -86,9 +86,10 @@ passport.deserializeUser((user: Express.User, done) => {
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 })
 app.use(express.json());
