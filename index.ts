49142ffb91d0e96d22e4641f23a30b5407b115cf
Borek Bandell <borekbandell@hotmail.com>
2024-05-28 13:08:08 +0200

    Added cors for local/deployed

--- index.ts
+++ index.ts
@@ -86,7 +86,9 @@ passport.deserializeUser((user: Express.User, done) => {
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  // Deployed version:
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
